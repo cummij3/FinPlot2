bbb3dccab7625de15c4b165aa0917aa2cc8e35d9
JedCDephy <jcummings@dephy.com>
2022-01-30 08:29:23 -0800

    changes made

--- account.py
+++ account.py
@@ -7,18 +7,17 @@ Account class that allows you to input data to an account
 class Account():
 	def __init__(self, name):
 		self.name = name
-		self.data = {}
-		self.fields = ['date', 'balance', 'contribuition']
+		self.data = dict()
+		self.fields = ['ending balance', 'contribution']
+		self.tags = list()
 
 	def add_data(self, date, val_dict):
 		"""
 		add data
 
-		date format is yyyymmdd
-		val_dict format is {key1:value1, ...}
+		val_dict = {field1: val1, ...}
 		"""
-
-		self.data['date'] = val_dict
+		self.data[date] = val_dict
 
 	def get_data(self):
 		""" return data """
@@ -31,3 +30,11 @@ class Account():
 	def get_fields(self):
 		""" return fields """
 		return self.fields
+
+	def add_tag(self, tag):
+		""" add a tag to the account """
+		self.tags.append(tag)
+
+	def get_tags(self):
+		""" get account tags """
+		return self.tags
